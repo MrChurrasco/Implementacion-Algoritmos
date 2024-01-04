bb91522d89f93e45c20b57d8a6fca973d87c1e74
MrChurrasco <s.carrasco.1@ing.uchile.cl>
2024-01-04 17:10:02 -0300

    - Se optimiza y se documenta el código de FuncionesCosto.py

--- FuncionesCosto.py
+++ FuncionesCosto.py
@@ -1,113 +1,220 @@
 import math
+import numpy as np
 
-def sign (a):
-    if a<0:
-        return -1
-    elif a>0:
-        return 1
-    else:
-        return 0
 
-def R (z, s):
-    r = max(0, 1-z)
-    r = min (1-s, r)
-    return r
+# Funciones costo
+# Square Loss
+def square_loss(f: float, y: int) -> float:
+    """
+    Calcula el error cuadrático de la funcion f con respecto al valor de y.
 
-#Funciones costo
-#Square Loss
-def square (f, y):
-    V=(y-f)**2
-    return V
+    Args:
+        f (float): Predicción del valor.
+        y (int): Valor real {-1,1}.
 
-#Hinge Loss
-def hinge (f,y):
-    V=max(0,1-y*f)
-    return V
+    Returns: El error cuadrático de la función f con el valor y.
 
-#Smoothed Hinge Loss
-def smoothinge (f,y):
-    a=y*f
-    if a>=1:
-        V=0
-    elif a>0 and a<=1:
-        V=(1-a)**2
-        V=V/2
-    elif a<=0:
-        V=0.5-a
-    return V
+    """
+    return math.pow((y - f), 2)
 
-#Modified Square Loss
-def modsquare (f,y):
-    V=max(1-y*f,0)
-    V=V**2
-    return V
 
-#Exponential Loss
-def exp (f,y):
-    V=math.exp(-y*f)
-    return V
+# Hinge Loss
+def hinge_loss(f: float, y: int) -> float:
+    """
+    Calcula el error de Hinge de la funcion f con respecto al valor de y.
 
-#Log loss
-def log (f,y):
-    V=math.log(1+math.exp(-y*f))
-    return V
+    Args:
+        f (float): Predicción del valor.
+        y (int): Valor real {-1,1}.
 
-#Based on Sigmoid Loss
-def sigmoid (f,y,gamma,theta,l):
-    a=y*f
-    if a>=-1 and a<=0:
-        V=(1.2-gamma)-gamma*a
-    elif a>0 and a<=theta:
-        b=(1.2-2*gamma)*a
-        b=b/theta
-        V=(1.2-l)-b
-    elif a>theta and a<=1:
-        b=gamma/(1-theta)
-        c=l*a
-        c=c/(1-theta)
-        V=b-c
-    return V
+    Returns: El error de Hinge de la función f con el valor y.
 
-#Phi-Learning
-def phi (f,y):
-    a=y*f
-    if a>=0 and a<=1:
-        V=1-a
-    else:
-        V=1-sign(a)
-    return V
+    """
+    return max(1 - y * f, 0)
 
-#Ramp Loss
-def ramp (f, y, s, c):
-    V = R(f*y, s)+ R(-f*y, s) + c
-    return V
 
-#Smooth non-convex loss
-def smoothnonconvex (f,y,l):
-    V = 1 - math.tanh(l*y*f)
-    return V
+# Smoothed Hinge Loss
+def smooth_hinge_loss(f: float, y: int) -> float:
+    """
+    Calcula el error de Smooth Hinge de la funcion f con respecto al valor de y.
 
-#2-layer Neural New-works
-def layer (f,y):
-    V = 1 + math.exp(-y*f)
-    V=1/V
-    V = (1-V)**2
-    return V
+    Args:
+        f (float): Predicción del valor.
+        y (int): Valor real {-1,1}.
 
-#Logistic difference Loss
-def logistic (f,y,mu):
-    a = math.log(1 + math.exp(-y*f))
-    b = math.log(1 + math.exp(-y*f-mu))
-    V=a-b
-    return V
+    Returns: El error de Smooth Hinge de la función f con el valor y.
 
-#Smoothed 0-1 Loss
-def smooth01 (t):
-    if t>1:
-        V=0
-    elif t>=-1 and t<=1:
-        V=1/4*t**3-3/4*t+1/2
-    elif t<-1:
-        V=1
-    return V
-    
+    """
+    a = y * f
+    if 0 < a <= 1:
+        return math.pow((1 - a), 2) / 2
+    return (a <= 0) * (0.5 - a)
+
+
+# Modified Square Loss
+def mod_square_loss(f: float, y: int) -> float:
+    """
+    Calcula el Modified Square Loss de la funcion f con respecto al valor de y.
+
+    Args:
+        f (float): Predicción del valor.
+        y (int): Valor real {-1,1}.
+
+    Returns: El Modified Square Loss de la función f con el valor y.
+
+    """
+    return math.pow(max(1 - y * f, 0), 2)
+
+
+# Exponential Loss
+def exp_loss(f: float, y: int) -> float:
+    """
+    Calcula el Exponential Loss de la funcion f con respecto al valor de y.
+
+    Args:
+        f (float): Predicción del valor.
+        y (int): Valor real {-1,1}.
+
+    Returns: El Exponential Loss de la función f con el valor y.
+
+    """
+    return math.exp(-y * f)
+
+
+# Log loss
+def log_loss(f: float, y: int) -> float:
+    """
+    Calcula la Logistic Loss de la funcion f con respecto al valor de y.
+
+    Args:
+        f (float): Predicción del valor.
+        y (int): Valor real {-1,1}.
+
+    Returns: La Logistic Loss de la función f con el valor y.
+
+    """
+    return math.log(1 + math.exp(-y * f))
+
+
+# Based on Sigmoid Loss
+def sigmoid_loss(f: float, y: int, gamma: float, theta: float, lamb: float) -> float:
+    """
+    Calcula la Sigmoid Loss de la funcion f con respecto al valor de y.
+
+    Args:
+        f (float): Predicción del valor.
+        y (int): Valor real {-1,1}.
+        gamma (float): ... Parámetro Gamma del sigmoid.
+        theta (float): Debe estar entre (0,1). Parámetro Theta del sigmoid.
+        lamb (float): ... Parámetro Lambda del sigmoid.
+
+    Returns: La Sigmoid Loss de la función f con el valor y.
+
+    """
+    a = y * f
+    if -1 <= a <= 0:
+        return (1.2 - gamma) - gamma * a
+    elif 0 < a <= theta:
+        return (1.2 - lamb) - (1.2 - 2 * gamma) * a / theta
+    elif theta < a <= 1:
+        return (gamma - lamb * a) / (1 - theta)
+
+
+# Phi-Learning
+def phi_learning(f: float, y: int) -> float:
+    """
+    Calcula el Phi Learning de la funcion f con respecto al valor de y.
+
+    Args:
+        f (float): Predicción del valor.
+        y (int): Valor real {-1,1}.
+
+    Returns: El Phi Learning  de la función f con el valor y.
+
+    """
+    a = y * f
+    if 0 <= a <= 1:
+        return 1 - a
+    return 1 - np.sign(a)
+
+
+# Ramp Loss
+def ramp_loss(f: float, y: int, s: float, c: float) -> float:
+    """
+    Calcula la Ramp Loss de la funcion f con respecto al valor de y.
+
+    Args:
+        f (float): Predicción del valor.
+        y (int): Valor real {-1,1}.
+        s (float): Valor entre (-1,0].
+        c (float): Constante.
+
+    Returns: El Ramp Loss de la función f con el valor y.
+
+    """
+    return (min(1 - s, max(1 - f * y, 0)) +
+            min(1 - s, max(1 + f * y, 0)) + c)
+
+
+# Smooth non-convex loss
+def smooth_non_convex_loss(f: float, y: int, lamb: float) -> float:
+    """
+    Calcula la Smooth Non Convex Loss de la funcion f con respecto al valor de y.
+
+    Args:
+        f (float): Predicción del valor.
+        y (int): Valor real {-1,1}.
+        lamb (float): Constante.
+
+    Returns: La Smooth Non Convex Loss de la función f con el valor y.
+
+    """
+    return 1 - math.tanh(lamb * y * f)
+
+
+# 2-layer Neural New-works
+def layer_neural(f: float, y: int) -> float:
+    """
+    Calcula la 2-Layer Nueral New-Works Loss de la funcion f con respecto al valor de y.
+
+    Args:
+        f (float): Predicción del valor.
+        y (int): Valor real {-1,1}.
+
+    Returns: La 2-Layer Nueral New-Works Loss de la función f con el valor y.
+
+    """
+    return math.pow((1 - (1 / (1 + math.exp(-y * f)))), 2)
+
+
+# Logistic difference Loss
+def logistic(f: float, y: float, mu: float) -> float:
+    """
+    Calcula la Logistic Difference Loss de la funcion f con respecto al valor de y.
+
+    Args:
+        f (float): Predicción del valor.
+        y (int): Valor real {-1,1}.
+        mu (float): Valor controlado.
+
+    Returns: La Logistic Difference Loss de la función f con el valor y.
+
+    """
+    return ((math.log(1 + math.exp(-y * f))) -
+            (math.log(1 + math.exp(-y * f - mu))))
+
+
+# Smoothed 0-1 Loss
+def smooth01(t: float) -> float:
+    """
+    Calcula la Smoothed 0-1 Loss de la funcion f con respecto al valor de y.
+
+    Args:
+        t (float): Valor de yi(w*xi+b)
+
+    Returns: La Smoothed 0-1 Loss de la función f con el valor y.
+
+    """
+    if -1 <= t <= 1:
+        return (math.pow(t, 3) - 3 * t + 2) / 4
+    return t < -1
--- README.md
+++ README.md
@@ -2,18 +2,18 @@
 
  Implementación de algoritmos de optimización y diferentes funciones de convexas y no convexas.
 
-El proyecto esta en **Python 3.11.1**
+El proyecto esta en [**Python 3.11**](https://www.python.org/downloads/release/python-3117/).
 
 Para trabajar en este proyecto se debe instalar la versión antes dicha de Python y crear un entorno virtual. Para ello escriba el comando:
 
-```python-repl
-python -m venv .venv
+```python
+python -m venv venv
 ```
 
 Para instalar las librerías antes que se utilizan en este proyecto solo debe entrar con el comando:
 
-```python
-.venv\Scripts\activate
+```python   
+venv\Scripts\activate
 ```
 
 Ya dentro del entorno virtual en el cmd o powershell, escriba:
