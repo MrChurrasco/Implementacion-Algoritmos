3bbd0f884ea941b84dcc17e098592f7985571d14
MrChurrasco <sebastian.carrasco.olivero@gmail.com>
2024-01-10 20:26:14 -0300

    Semi-Newton algorithm complete
    - Se completa la implementación del algoritmo regularized coderivative-based damped semi-Newton algorithm

--- semi_newton/raw.py
+++ semi_newton/raw.py
@@ -1,77 +1,62 @@
 import numpy as np
+from scipy.optimize import minimize, Bounds
 from numpy.linalg import norm
 
-def backtracking(fun, grad_fun, xk: np.ndarray[float], pk: np.ndarray, alpha: float = 1, beta: float = 0.5,
-                 c: float = 1e-4) -> float:
-    """Método para calcular $\alpha$, usando las condiciones de Goldstein y el método de backtracking.
 
-    Args:
-        fun (function): Función por la cual se quiere descender.
-        xk (np.ndarray[float]): Valor de referencia para hacer el descenso.
-        grad_fun (function): El gradiente de la función 'fun'.
-        pk (np.ndarray): Vector que índica la dirección de descenso.
-        alpha (float, optional): Ponderador del vector pk. Defaults to 1.
-        beta (float, optional): Tasa de disminución del valor de alpha al no cumplir las condiciones de Goldstein.
-        Defaults to 0.5.
-        c (float, optional): Constante entre (0,1). Defaults to 1e-4.
-
-    Returns:
-        float: Retorna el primer $\alpha$ encontrado que cumple las condiciones de Goldstein.
-    """
-    # Calculamos las constantes del ciclo antes (Optimización)
-    rhs = c * grad_fun(xk) @ pk
-    fxk = fun(xk)
-    while fun(xk + alpha * pk) > fxk + alpha * rhs:
-        alpha = beta * alpha
-    return alpha
-
-
-def alg1(wk, bk, c, rho_max):
-    """
-    while -c * norm(dk) < np.inner(wk, dk):
-        pk = np.random.rand(1) * rho  # Choose [0, rho_max]
-        dk = np.random.rand(x0.shape)
-        wk = -bk(dk) - pk * dk
-    """
-    return 1
-
-
-def trial_step(fun, grad_fun, xk, dk, t0_bar: float, gamma: float,
-               t_min: float, beta, sigma):
-
-    t0 = backtracking(fun, grad_fun, xk, dk, t0_bar, beta, sigma)
-    t1_bar = max(t0, t_min)
-    t1 = backtracking(fun, grad_fun, xk, dk, t1_bar, beta, sigma)
-    t2_bar = 0
-    while True:
-        if t0 == t0_bar and t1 == t1_bar:
-            t2_bar = gamma*t1
-        else:
-            t2_bar = max(t1, t_min)
-        t2 = backtracking(fun, grad_fun, xk, dk, t2_bar, beta, sigma)
-        if t2 == t2_bar:
-            break
-        t0_bar = t1_bar
-        t0 = t1
-        t1_bar = t2_bar
-        t1 = t2
-    return t2
-
-
-def semi_newton_paso(fun, grad_fun, bk, xk: np.ndarray[float], beta: float,
+def semi_newton_paso(fun: callable, grad_fun: callable, bk: callable,
+                     xk: np.ndarray[float], beta: float,
                      c: float, t_min: float, t0_bar: float, gamma: float,
                      rho: float, sigma: float) -> np.ndarray[float]:
     wk = grad_fun(xk)
-    d_k = alg1(wk, bk, c, rho)
-    t_k = trial_step(t0_bar=t0_bar,
-                     gamma=gamma,
-                     t_min=t_min,
-                     dk=d_k,
-                     fun=fun,
-                     grad_fun=grad_fun,
-                     xk=xk,
-                     beta=beta,
-                     sigma=sigma
-                     )
 
-    return xk + t_k * d_k
+    # Obteniendo dk
+    # Para ello, minimizaremos la norma del vector
+    # Obteniendo el vector dk con la dirección de descenso
+    # con un vector casi unitario
+
+    # Posicion inicial
+    x0 = -wk / c  # Cota desigualdad <wk,dk> <=-C||dk||^2
+    x0 = x0.append(rho)  # hat{x0} = [x0 rho_max]
+
+    sol = minimize(fun=lambda x: norm(x[:-1]),  # ||d||^2
+                   x0=x0,
+                   constraints=(
+                       {"type": "eq",
+                        "fun": lambda x: bk(x[:-1]) + x[-1] * x[:-1] - wk},  # Bk(dk) + rho_kdk = wk
+                       {"type": "ineq",
+                        "fun": lambda x: -(np.inner(wk, x[:-1]) + c * norm(x[:-1]))},  # <wk,dk> <=-C||dk||^2
+                       {"type": "ineq",
+                        "fun": lambda x: norm(x[:-1]) - 1}),  # ||d||^2 >= 1
+                   method="trust-constr",
+                   bounds=[Bounds(lb=1e-10) for _ in range(wk.shape[0])] + [Bounds(0, rho)],
+                   )
+
+    dk = sol.x[:-1]  # Obtenemos el vector dkque cumple con las condiciones
+
+    # Backtracking + trial step
+    tau = np.zeros(3)  # tau[-i] = tau_(k-i)
+    tau_bar = np.zeros(3)  # tau_bar[-i] = \bar(tau)_(k-i)
+
+    tau_bar[-2] = t0_bar
+
+    # Condición de backtracking: phi(xk + tau_k*dk) > phi(xk)+sigma*tau_k*<wk,dk>
+    backtracking_cond = lambda t: fun(xk + t * dk) > fun(xk) + sigma * t * np.inner(wk, dk)
+
+    # Condiciones iniciales para el loop
+    tau[-2] = beta * t0_bar if backtracking_cond(t0_bar) else t0_bar
+    tau_bar[-1] = max([tau[-2], t_min])
+    tau[-1] = beta * tau_bar[-1] if backtracking_cond(tau_bar[-1]) else tau_bar[-1]
+
+    # Ciclo backtracking
+    while True:
+        # Si tau_(k-2) = tau_bar_(k-2) y tau_(k-1) = tau_bar_(k-1)
+        # tau_bar_k = gamma*tau_(k-1)
+        # else -> tau_bar_k = max{tau_(k-1), t_min}
+        tau_bar[0] = max([tau_bar[-1], t_min]) if False in (tau[1:] == tau_bar[1:]) else gamma * tau[-1]
+        if not backtracking_cond(tau_bar[0]):
+            break
+        tau[0] = beta * tau_bar[0]  # tau_k = beta*tau_bar_k
+        tau[-2:] = tau[:-1]  # tau_k-1 = tau_k y tau_k-2 = tau_k-1
+        tau_bar[-2:] = tau_bar[:-1]  # tau_bar_k-1 = tau_bar_k y tau_bar_k-2 = tau_bar_k-1
+
+    return xk + tau[0] * dk
