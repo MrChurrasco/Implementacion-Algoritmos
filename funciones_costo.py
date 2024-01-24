a9b3e14e3db12694b94735999f66ec3b44a3f409
MrChurrasco <sebastian.carrasco.olivero@gmail.com>
2024-01-24 17:43:02 -0300

    Refactorización de las funciones de costo
    - Las funciones de costo ahora son encapsuladas.
    - Los test también fueron adaptados para aceptar la nueva forma de recibir los parámetros de la función.

--- funciones_costo.py
+++ funciones_costo.py
@@ -1,9 +1,9 @@
 import math
 import numpy as np
 from numpy.linalg import norm
-from numba import vectorize, float64
+from numba import vectorize, float64, int32
 
-list_types_common = [float64(float64)]
+list_types_common = [float64(float64, int32)]
 
 
 # Función Objetivo
@@ -11,7 +11,7 @@ list_types_common = [float64(float64)]
 # Funciones costo
 # Square Loss
 @vectorize(list_types_common, nopython=True)
-def square_loss(t: float) -> float:
+def square_loss(t: float, derivative: int) -> float:
     """
     Calcula el error cuadrático del valor t con respecto al valor de y.
 
@@ -21,21 +21,18 @@ def square_loss(t: float) -> float:
     Returns: El error cuadrático de la función f con el valor y.
 
     """
-    return (1 - t) ** 2
-
-
-@vectorize(list_types_common, nopython=True)
-def derivative_square_loss(t: float):
-    return 2 * (t - 1)
-
-
-def dderivative():
-    pass
+    if derivative == 0:
+        return (1 - t) ** 2
+    elif derivative == 1:
+        return 2 * (t - 1)
+    elif derivative == 2:
+        return 2.
+    return 0.
 
 
 # Hinge Loss
 @vectorize(list_types_common, nopython=True)
-def hinge_loss(t: float) -> float:
+def hinge_loss(t: float, derivative: int) -> float:
     """
     Calcula el error de Hinge de la funcion f con respecto al valor de y.
 
@@ -46,17 +43,16 @@ def hinge_loss(t: float) -> float:
     Returns: El error de Hinge de la función f con el valor y.
 
     """
-    return max(1 - t, 0.)
-
-
-@vectorize(list_types_common, nopython=True)
-def derivative_hinge_loss(t: float) -> float:
-    return -(t < 1)
+    if derivative == 0:
+        return max(1 - t, 0.)
+    elif derivative == 1:
+        return -(t < 1)
+    return 0.
 
 
 # Smoothed Hinge Loss
 @vectorize(list_types_common, nopython=True)
-def smooth_hinge_loss(t: float) -> float:
+def smooth_hinge_loss(t: float, derivative: int) -> float:
     """
     Calcula el error de Smooth Hinge de la funcion f con respecto al valor de y.
 
@@ -67,21 +63,22 @@ def smooth_hinge_loss(t: float) -> float:
     Returns: El error de Smooth Hinge de la función f con el valor y.
 
     """
-    if 0 < t <= 1:
-        return (1 - t) ** 2 / 2
-    return (t <= 0) * (0.5 - t)
-
-
-@vectorize(list_types_common, nopython=True)
-def derivative_smooth_hinge_loss(t: float) -> float:
-    if 0 < t < 1:
-        return t - 1
-    return -(t <= 0)
+    if derivative == 0:
+        if 0 < t <= 1:
+            return (1 - t) ** 2 / 2
+        return (t <= 0) * (0.5 - t)
+    elif derivative == 1:
+        if 0 < t < 1:
+            return t - 1
+        return -(t <= 0)
+    elif derivative == 2:
+        return 0 < t < 1
+    return 0.
 
 
 # Modified Square Loss
 @vectorize(list_types_common, nopython=True)
-def mod_square_loss(t: float) -> float:
+def mod_square_loss(t: float, derivative: int) -> float:
     """
     Calcula el Modified Square Loss de la funcion f con respecto al valor de y.
 
@@ -92,17 +89,18 @@ def mod_square_loss(t: float) -> float:
     Returns: El Modified Square Loss de la función f con el valor y.
 
     """
-    return max(1 - t, 0) ** 2
-
-
-@vectorize(list_types_common, nopython=True)
-def derivative_mod_square_loss(t: float) -> float:
-    return (t < 1) * 2 * (t - 1)
+    if derivative == 0:
+        return max(1 - t, 0) ** 2
+    elif derivative == 1:
+        return (t < 1) * 2 * (t - 1)
+    elif derivative == 2:
+        return 2 * (t < 1)
+    return 0.
 
 
 # Exponential Loss
 @vectorize(list_types_common, nopython=True)
-def exp_loss(t: float) -> float:
+def exp_loss(t: float, derivative: int) -> float:
     """
     Calcula el Exponential Loss de la funcion f con respecto al valor de y.
 
@@ -112,17 +110,12 @@ def exp_loss(t: float) -> float:
     Returns: El Exponential Loss de la función f con el valor y.
 
     """
-    return math.exp(-t)
-
-
-@vectorize(list_types_common, nopython=True)
-def derivative_exp_loss(t: float) -> float:
-    return -math.exp(-t)
+    return math.exp(-t) * math.pow(-1, derivative)
 
 
 # Log loss
 @vectorize(list_types_common, nopython=True)
-def log_loss(t: float) -> float:
+def log_loss(t: float, derivative: int) -> float:
     """
     Calcula la Logistic Loss de la funcion f con respecto al valor de y.
 
@@ -133,21 +126,21 @@ def log_loss(t: float) -> float:
     Returns: La Logistic Loss de la función f con el valor y.
 
     """
-    return math.log(1 + math.exp(-t))
-
-
-@vectorize(list_types_common, nopython=True)
-def derivative_log_loss(t: float) -> float:
-    return -1 / (1 + math.exp(t))
+    if derivative == 0:
+        return math.log(1 + math.exp(-t))
+    elif derivative == 1:
+        return -1 / (1 + math.exp(t))
+    return math.exp(t) / (1 + math.exp(t))**2
 
 
 # Based on Sigmoid Loss
 # Falta información de los parametros para el testeo
-@vectorize([float64(float64, float64, float64, float64)], nopython=True)
+@vectorize([float64(float64, float64, float64, float64, int32)], nopython=True)
 def sigmoid_loss(t: float,
                  gamma: float,
                  theta: float,
-                 lamb: float) -> float:
+                 lamb: float,
+                 derivative: int) -> float:
     """
     Calcula la Sigmoid Loss de la funcion f con respecto al valor de y.
 
@@ -161,43 +154,26 @@ def sigmoid_loss(t: float,
     Returns: La Sigmoid Loss de la función f con el valor y.
 
     """
-    if -1 <= t <= 0:
-        return (1.2 - gamma) - gamma * t
-    elif 0 < t <= theta:
-        return (1.2 - lamb) - (1.2 - 2 * gamma) * t / theta
-    else:
-        return (gamma - lamb * t) / (1 - theta)
-
-
-@vectorize([float64(float64, float64, float64, float64)], nopython=True)
-def derivative_sigmoid_loss(t: float,
-                            gamma: float,
-                            theta: float,
-                            lamb: float) -> float:
-    """
-    Calcula la Sigmoid Loss de la funcion f con respecto al valor de y.
-
-    Args:
-        f (float | np.floating): Predicción del valor. Debe pertenece el valor entre los valores [-1,1].
-        y (int | np.integer): Valor real {-1,1}.
-        gamma (float | np.floating): ... Parámetro Gamma del sigmoid.
-        theta (float | np.floating): Debe estar entre (0,1). Parámetro Theta del sigmoid.
-        lamb (float | np.floating): ... Parámetro Lambda del sigmoid.
-
-    Returns: La Sigmoid Loss de la función f con el valor y.
-
-    """
-    if -1 <= t <= 0:
-        return - gamma
-    elif 0 < t <= theta:
-        return (2 * gamma - 1.2) / theta
-    else:
-        return lamb / (1 - theta)
+    if derivative == 0:
+        if -1 <= t <= 0:
+            return (1.2 - gamma) - gamma * t
+        elif 0 < t <= theta:
+            return (1.2 - lamb) - (1.2 - 2 * gamma) * t / theta
+        else:
+            return (gamma - lamb * t) / (1 - theta)
+    elif derivative == 1:
+        if -1 <= t <= 0:
+            return - gamma
+        elif 0 < t <= theta:
+            return (2 * gamma - 1.2) / theta
+        else:
+            return lamb / (1 - theta)
+    return 0.
 
 
 # Phi-Learning
 @vectorize(list_types_common, nopython=True)
-def phi_learning(t: float) -> float:
+def phi_learning(t: float, derivative: int) -> float:
     """
     Calcula el Phi Learning de la funcion f con respecto al valor de y.
 
@@ -208,12 +184,17 @@ def phi_learning(t: float) -> float:
     Returns: El Phi Learning de la función f con el valor y.
 
     """
-    return 1 - t if 0 <= t <= 1 else 1 - np.sign(t)
+    if derivative == 0:
+        return 1. - t if 0 <= t <= 1 else 1. - np.sign(t)
+    elif derivative == 1:
+        return -1.
+    return 0.
+
 
 
 # Ramp Loss
-@vectorize([float64(float64, float64, float64)], nopython=True)
-def ramp_loss(t: float, s: float, c: float) -> float:
+@vectorize([float64(float64, float64, float64, int32)], nopython=True)
+def ramp_loss(t: float, s: float, c: float, derivative: int) -> float:
     """
     Calcula la Ramp Loss de la funcion f con respecto al valor de y.
 
@@ -226,13 +207,15 @@ def ramp_loss(t: float, s: float, c: float) -> float:
     Returns: El Ramp Loss de la función f con el valor y.
 
     """
-    return (min(1 - s, max(1 - t, 0)) +
-            min(1 - s, max(1 + t, 0)) + c)
+    if derivative == 0:
+        return (min(1 - s, max(1 - t, 0)) +
+                min(1 - s, max(1 + t, 0)) + c)
+    return 0.
 
 
 # Smooth non-convex loss
-@vectorize([float64(float64, float64)], nopython=True)
-def smooth_non_convex_loss(t: float, lamb: float) -> float:
+@vectorize([float64(float64, float64, int32)], nopython=True)
+def smooth_non_convex_loss(t: float, lamb: float, derivative: int) -> float:
     """
     Calcula la Smooth Non Convex Loss de la funcion f con respecto al valor de y.
 
@@ -244,17 +227,16 @@ def smooth_non_convex_loss(t: float, lamb: float) -> float:
     Returns: La Smooth Non Convex Loss de la función f con el valor y.
 
     """
-    return 1 - math.tanh(lamb * t)
-
-
-@vectorize([float64(float64, float64)], nopython=True)
-def derivative_smooth_non_convex_loss(t: float, lamb: float) -> float:
-    return -lamb * (1 / np.cosh(lamb * t) ** 2)
+    if derivative == 0:
+        return 1 - math.tanh(lamb * t)
+    elif derivative == 1:
+        return -lamb * (1 / np.cosh(lamb * t) ** 2)
+    return 2 * ((lamb / np.cosh(lamb * t)) ** 2) * math.tanh(lamb * t)
 
 
 # 2-layer Neural New-works
 @vectorize(list_types_common, nopython=True)
-def layer_neural(t: float) -> float:
+def layer_neural(t: float, derivative: int) -> float:
     """
     Calcula la 2-Layer Nueral New-Works Loss de la funcion f con respecto al valor de y.
 
@@ -265,18 +247,17 @@ def layer_neural(t: float) -> float:
     Returns: La 2-Layer Nueral New-Works Loss de la función f con el valor y.
 
     """
-    return (1 - (1 / (1 + math.exp(-t)))) ** 2
-
-
-@vectorize(list_types_common, nopython=True)
-def derivative_layer_neural(t: float) -> float:
     e_t = np.exp(t)
-    return - (2 * e_t) / (1 + e_t) ** 3
+    if derivative == 0:
+        return 1 / ((1 + e_t) ** 2)
+    elif derivative == 1:
+        return (-2 * e_t) / ((1 + e_t) ** 3)
+    return ((-2 * e_t) * (-2 * e_t - 1)) / ((1 + e_t) ** 4)
 
 
 # Logistic difference Loss
-@vectorize([float64(float64, float64)], nopython=True)
-def logistic_difference_loss(t: float, mu: float) -> float:
+@vectorize([float64(float64, float64, int32)], nopython=True)
+def logistic_difference_loss(t: float, mu: float, derivative: int) -> float:
     """
     Calcula la Logistic Difference Loss de la funcion f con respecto al valor de y.
 
@@ -288,17 +269,16 @@ def logistic_difference_loss(t: float, mu: float) -> float:
     Returns: La Logistic Difference Loss de la función f con el valor y.
 
     """
-    return (math.log(1 + math.exp(-t))) - (math.log(1 + math.exp(-t - mu)))
-
-
-@vectorize([float64(float64, float64)], nopython=True)
-def derivative_logistic_difference_loss(t: float, mu: float) -> float:
-    return (- 1 / (1 + math.exp(t))) + (1 / (1 + math.exp(t + mu)))
+    if derivative == 0:
+        return (math.log(1 + math.exp(-t))) - (math.log(1 + math.exp(-t - mu)))
+    elif derivative == 1:
+        return (- 1 / (1 + math.exp(t))) + (1 / (1 + math.exp(t + mu)))
+    return (math.exp(t) / ((1 + math.exp(t))**2)) + (-(1 + math.exp(t + mu)) / ((1 + math.exp(t + mu))**2))
 
 
 # Smoothed 0-1 Loss
 @vectorize(list_types_common, nopython=True)
-def smooth01(t: float) -> float:
+def smooth01(t: float, derivative: int) -> float:
     """
     Calcula la Smoothed 0-1 Loss de la funcion f con respecto al valor de y.
 
@@ -308,9 +288,12 @@ def smooth01(t: float) -> float:
     Returns: La Smoothed 0-1 Loss de la función f con el valor y.
 
     """
-    return (t ** 3 - 3 * t + 2) / 4 if -1 <= t <= 1 else t < -1
-
-
-@vectorize(list_types_common, nopython=True)
-def derivative_smooth01(t: float) -> float:
-    return (-1 <= t <= 1) * ((3 / 4) * (t ** 2 - 1))
+    if derivative == 0:
+        return (t ** 3 - 3 * t + 2) * 0.25 if -1 <= t <= 1 else t < -1
+    elif derivative == 1:
+        return (-1 <= t <= 1) * 0.75 * (t ** 2 - 1)
+    elif derivative == 2:
+        return (-1 < t < 1) * 1.5 * t
+    elif derivative == 3:
+        return (-1 < t < 1) * 1.5
+    return 0.
--- tests/test_funciones_costo.py
+++ tests/test_funciones_costo.py
@@ -3,138 +3,201 @@ import pytest
 import numpy as np
 
 ##
-rango_test = np.linspace(-10, 10, 100, dtype=float)
+rango_test = np.linspace(-10, 10, 21, dtype=float)
 precision = 1e-15
 
 
 # Square Loss
 @pytest.mark.parametrize(
-    "t_test",
-    rango_test
-)
-def test_square_loss_positive_always(t_test):
-    assert fc.square_loss(t_test) > 0
-
-
-@pytest.mark.parametrize(
     "t_test, expected",
-    [(t, (1 - t) ** 2)
+    [((t, 0), (1 - t) ** 2)
      for t in rango_test]
 )
 def test_square_loss(t_test, expected):
-    assert fc.square_loss(t_test) - expected < precision
+    assert fc.square_loss(*t_test) - expected < precision
 
 
 @pytest.mark.parametrize(
     "t_test, expected",
-    [(t, 2 * (t-1))
+    [((t, 1), 2 * (t - 1))
      for t in rango_test]
 )
 def test_derivative_square_loss(t_test, expected):
-    assert fc.derivative_square_loss(t_test) - expected < precision
+    assert fc.square_loss(*t_test) - expected < precision
+
+
+@pytest.mark.parametrize(
+    "t_test, expected",
+    [((t, 2), 2.)
+     for t in rango_test]
+)
+def test_derivative2_square_loss(t_test, expected):
+    assert fc.square_loss(*t_test) - expected < precision
+
+
+@pytest.mark.parametrize(
+    "t_test, expected",
+    [((t, n), 0.)
+     for t in rango_test
+     for n in range(3, 10)]
+)
+def test_derivative_n_square_loss(t_test, expected):
+    assert fc.square_loss(*t_test) - expected < precision
 
 
 # Hinge Loss
 @pytest.mark.parametrize(
     "t_test, expected",
-    [(t, 1 - t) if t < 1
-     else (t, 0.)
+    [((t, 0), 1 - t) if t < 1
+     else ((t, 0), 0.)
      for t in rango_test]
 )
 def test_hinge_loss(t_test, expected):
-    assert fc.hinge_loss(t_test) - expected < precision
+    assert fc.hinge_loss(*t_test) - expected < precision
 
 
 @pytest.mark.parametrize(
     "t_test, expected",
-    [(t, - 1) if t < 1
-     else (t, 0.)
+    [((t, 1), - 1.) if t < 1
+     else ((t, 1), 0.)
      for t in rango_test]
 )
 def test_derivative_hinge_loss(t_test, expected):
-    assert fc.derivative_hinge_loss(t_test) - expected < precision
+    assert fc.hinge_loss(*t_test) - expected < precision
+
+
+@pytest.mark.parametrize(
+    "t_test, expected",
+    [((t, n), 0.)
+     for t in rango_test
+     for n in range(3, 10)]
+)
+def test_derivative_n_hinge_loss(t_test, expected):
+    assert fc.hinge_loss(*t_test) - expected < precision
 
 
 # Smoothed Hinge Loss
 @pytest.mark.parametrize(
     "t_test, expected",
-    [(t, 0.5 - t) if t <= 0 else
-     (t, 0) if t >= 1 else
-     (t, (1 - t) ** 2 / 2)
+    [((t, 0), 0.5 - t) if t <= 0 else
+     ((t, 0), 0) if t >= 1 else
+     ((t, 0), (1 - t) ** 2 / 2)
      for t in rango_test]
 )
 def test_smooth_hinge_loss(t_test, expected):
-    assert fc.smooth_hinge_loss(t_test) - expected < precision
+    assert fc.smooth_hinge_loss(*t_test) - expected < precision
 
 
 @pytest.mark.parametrize(
     "t_test, expected",
-    [(t, -1) if t <= 0 else
-     (t, 0) if t >= 1 else
-     (t, t - 1)
+    [((t, 1), -1) if t <= 0 else
+     ((t, 1), 0) if t >= 1 else
+     ((t, 1), t - 1)
      for t in rango_test]
 )
 def test_derivate_smooth_hinge_loss(t_test, expected):
-    assert fc.derivative_smooth_hinge_loss(t_test) - expected < precision
+    assert fc.smooth_hinge_loss(*t_test) - expected < precision
+
+
+@pytest.mark.parametrize(
+    "t_test, expected",
+    [((t, 2), 0.) if t <= 0 else
+     ((t, 2), 0.) if t >= 1 else
+     ((t, 2), 1.)
+     for t in rango_test]
+)
+def test_derivate2_smooth_hinge_loss(t_test, expected):
+    assert fc.smooth_hinge_loss(*t_test) - expected < precision
+
+
+@pytest.mark.parametrize(
+    "t_test, expected",
+    [((t, n), 0.)
+     for t in rango_test
+     for n in range(3, 10)]
+)
+def test_derivative_n_smooth_hinge_loss(t_test, expected):
+    assert fc.smooth_hinge_loss(*t_test) - expected < precision
 
 
 # Modified Square Loss
 @pytest.mark.parametrize(
     "t_test, expected",
-    [(t, (1 - t) ** 2) if 1 >= t else
-     (t, 0)
+    [((t, 0), (1 - t) ** 2) if 1 >= t else
+     ((t, 0), 0)
      for t in rango_test]
 )
 def test_mod_square_loss(t_test, expected):
-    assert fc.mod_square_loss(t_test) - expected < precision
+    assert fc.mod_square_loss(*t_test) - expected < precision
 
 
 @pytest.mark.parametrize(
     "t_test, expected",
-    [(t, 2 * (t - 1)) if 1 >= t else
-     (t, 0)
+    [((t, 1), 2 * (t - 1)) if 1 >= t else
+     ((t, 1), 0)
      for t in rango_test]
 )
 def test_derivative_mod_square_loss(t_test, expected):
-    assert fc.derivative_mod_square_loss(t_test) - expected < precision
+    assert fc.mod_square_loss(*t_test) - expected < precision
+
+
+@pytest.mark.parametrize(
+    "t_test, expected",
+    [((t, 2), 2.) if 1 > t else
+     ((t, 2), 0.)
+     for t in rango_test]
+)
+def test_derivative2_mod_square_loss(t_test, expected):
+    assert fc.mod_square_loss(*t_test) - expected < precision
+
+
+@pytest.mark.parametrize(
+    "t_test, expected",
+    [((t, n), 0.)
+     for t in rango_test
+     for n in range(3, 10)]
+)
+def test_derivative_n_smooth_hinge_loss(t_test, expected):
+    assert fc.mod_square_loss(*t_test) - expected < precision
 
 
 # Exponential Loss
 @pytest.mark.parametrize(
     "t_test, expected",
-    [(t, np.exp(-t))
+    [((t, n), np.exp(-t)*(-1)**2)
+     for n in range(3, 10)
      for t in rango_test]
 )
 def test_exp_loss(t_test, expected):
-    assert fc.exp_loss(t_test) - expected < precision
-
-
-@pytest.mark.parametrize(
-    "t_test, expected",
-    [(t, -np.exp(-t))
-     for t in rango_test]
-)
-def test_derivative_exp_loss(t_test, expected):
-    assert fc.derivative_exp_loss(t_test) - expected < precision
+    assert fc.exp_loss(*t_test) - expected < precision
 
 
 # Log loss
 @pytest.mark.parametrize(
     "t_test, expected",
-    [(t, np.log(1 + np.exp(-t)))
+    [((t, 0), np.log(1 + np.exp(-t)))
      for t in rango_test]
 )
 def test_log_loss(t_test, expected):
-    assert fc.log_loss(t_test) - expected < precision
+    assert fc.log_loss(*t_test) - expected < precision
 
 
 @pytest.mark.parametrize(
     "t_test, expected",
-    [(t, -1/ (1 + np.exp(t)))
+    [((t, 1), -1 / (1 + np.exp(t)))
      for t in rango_test]
 )
 def test_derivative_log_loss(t_test, expected):
-    assert fc.derivative_log_loss(t_test) - expected < precision
+    assert fc.log_loss(*t_test) - expected < precision
+
+
+@pytest.mark.parametrize(
+    "t_test, expected",
+    [((t, 2), np.exp(t) / ((1 + np.exp(t))**2))
+     for t in rango_test]
+)
+def test_derivative2_log_loss(t_test, expected):
+    assert fc.log_loss(*t_test) - expected < precision
 
 
 """
@@ -168,108 +231,197 @@ def test_sigmoid_loss(fx_test,
 # Phi-Learning
 @pytest.mark.parametrize(
     "t_test, expected",
-    [(t, 1 - t) if 0 <= t <= 1 else
-     (t, 1 - np.sign(t))
+    [((t, 0), 1 - t) if 0 <= t <= 1 else
+     ((t, 0), 1 - np.sign(t))
      for t in rango_test]
 )
 def test_phi_learning(t_test, expected):
-    assert fc.phi_learning(t_test) - expected < precision
+    assert fc.phi_learning(*t_test) - expected < precision
+
+
+@pytest.mark.parametrize(
+    "t_test, expected",
+    [((t, 1), - 1.)
+     for t in rango_test]
+)
+def test_derivative_phi_learning(t_test, expected):
+    assert fc.phi_learning(*t_test) - expected < precision
+
+
+@pytest.mark.parametrize(
+    "t_test, expected",
+    [((t, n), 0.)
+     for n in range(2, 10)
+     for t in rango_test]
+)
+def test_derivative_n_phi_learning(t_test, expected):
+    assert fc.phi_learning(*t_test) - expected < precision
 
 
 # Ramp Loss
 @pytest.mark.parametrize(
-    "t_test, s_test, c_test, expected",
-    [(t, s, c, (min(1 - s, max(1 - t, 0)) + min(1 - s, max(1 + t, 0)) + c))
+    "test, expected",
+    [((t, s, c, 0), min(1 - s, max(1 - t, 0)) + min(1 - s, max(1 + t, 0)) + c)
      for s in np.linspace(-1 + 1e-10, 0, 25, dtype=float)
      for c in rango_test
      for t in rango_test]
 )
-def test_ramp_loss(t_test, s_test, c_test, expected):
-    assert fc.ramp_loss(t_test, s_test, c_test) - expected < precision
+def test_ramp_loss(test, expected):
+    assert fc.ramp_loss(*test) - expected < precision
+
+
+@pytest.mark.parametrize(
+    "t_test, expected",
+    [((t, s, c, n), 0.)
+     for s in np.linspace(-1 + 1e-10, 0, 25, dtype=float)
+     for c in rango_test
+     for n in range(1, 5)
+     for t in rango_test]
+)
+def test_derivative_n_ramp_loss(t_test, expected):
+    assert fc.ramp_loss(*t_test) - expected < precision
 
 
 # Smooth non-convex loss
 @pytest.mark.parametrize(
-    "t_test, lamb_test, expected",
-    [(t, lamb, 1 - np.tanh(lamb * t))
+    "test, expected",
+    [((t, lamb, 0), 1 - np.tanh(lamb * t))
      for lamb in np.linspace(-1 + 1e-10, 1, 25, dtype=float)
      for t in rango_test]
 )
-def test_smooth_non_convex_loss(t_test, lamb_test, expected):
-    assert fc.smooth_non_convex_loss(t_test, lamb_test) - expected < precision
+def test_smooth_non_convex_loss(test, expected):
+    assert fc.smooth_non_convex_loss(*test) - expected < precision
 
 
 @pytest.mark.parametrize(
-    "t_test, lamb_test, expected",
-    [(t, lamb, - lamb * (1 / np.cosh(lamb * t)) ** 2)
+    "test, expected",
+    [((t, lamb, 1), - lamb * (1 / np.cosh(lamb * t)) ** 2)
      for lamb in np.linspace(-1 + 1e-10, 1, 25, dtype=float)
      for t in rango_test]
 )
-def test_derivative_smooth_non_convex_loss(t_test, lamb_test, expected):
-    assert fc.derivative_smooth_non_convex_loss(t_test, lamb_test) - expected < precision
+def test_derivative_smooth_non_convex_loss(test, expected):
+    assert fc.smooth_non_convex_loss(*test) - expected < precision
+
+
+@pytest.mark.parametrize(
+    "test, expected",
+    [((t, lamb, 2), 2 * ((lamb / np.cosh(lamb * t)) ** 2) * np.tanh(lamb * t))
+     for lamb in np.linspace(-1 + 1e-10, 1, 25, dtype=float)
+     for t in rango_test]
+)
+def test_derivative2_smooth_non_convex_loss(test, expected):
+    assert fc.smooth_non_convex_loss(*test) - expected < precision
 
 
 # 2-layer Neural New-works
 @pytest.mark.parametrize(
     "t_test, expected",
-    [(t, (1 - 1 / (1 + np.exp(-t))) ** 2)
+    [((t, 0), (1 - 1 / (1 + np.exp(-t))) ** 2)
      for t in rango_test]
 )
 def test_layer_neural(t_test, expected):
-    assert fc.layer_neural(t_test) - expected < precision
+    assert fc.layer_neural(*t_test) - expected < precision
 
 
 @pytest.mark.parametrize(
     "t_test, expected",
-    [(t, -(2 * np.exp(t)) / ((1 + np.exp(t)) ** 3))
+    [((t, 1), -(2 * np.exp(t)) / ((1 + np.exp(t)) ** 3))
      for t in rango_test]
 )
 def test_derivative_layer_neural(t_test, expected):
-    assert fc.derivative_layer_neural(t_test) - expected < precision
+    assert fc.layer_neural(*t_test) - expected < precision
+
+
+@pytest.mark.parametrize(
+    "t_test, expected",
+    [((t, 2), ((-2 * np.exp(t)) * (-2 * np.exp(t) - 1)) / ((1 + np.exp(t)) ** 4))
+     for t in rango_test]
+)
+def test_derivative2_layer_neural(t_test, expected):
+    assert fc.layer_neural(*t_test) - expected < precision
 
 
 # Logistic difference Loss
 @pytest.mark.parametrize(
-    "t_test, mu_test, expected",
-    [(t, mu, ((np.log(1 + np.exp(-t))) - (np.log(1 + np.exp(-t - mu)))))
+    "test, expected",
+    [((t, mu, 0), (np.log(1 + np.exp(-t))) - (np.log(1 + np.exp(-t - mu))))
      for mu in rango_test
      for t in rango_test]
 )
-def test_logistic_difference_loss(t_test, mu_test, expected):
-    assert fc.logistic_difference_loss(t_test, mu_test) - expected < precision
+def test_logistic_difference_loss(test, expected):
+    assert fc.logistic_difference_loss(*test) - expected < precision
 
 
-# Logistic difference Loss
 @pytest.mark.parametrize(
-    "t_test, mu_test, expected",
-    [(t, mu, ((1 / (1 + np.exp(t + mu))) - (1 / (1 + np.exp(t)))))
+    "test, expected",
+    [((t, mu, 1), (1 / (1 + np.exp(t + mu))) - (1 / (1 + np.exp(t))))
      for mu in rango_test
      for t in rango_test]
 )
-def test_derivative_logistic_difference_loss(t_test, mu_test, expected):
-    assert fc.derivative_logistic_difference_loss(t_test, mu_test) - expected < precision
+def test_derivative_logistic_difference_loss(test, expected):
+    assert fc.logistic_difference_loss(*test) - expected < precision
+
+
+@pytest.mark.parametrize(
+    "test, expected",
+    [((t, mu, 2), (np.exp(t) / ((1 + np.exp(t))**2)) + (-(1 + np.exp(t + mu)) / ((1 + np.exp(t + mu))**2)))
+     for mu in rango_test
+     for t in rango_test]
+)
+def test_derivative2_logistic_difference_loss(test, expected):
+    assert fc.logistic_difference_loss(*test) - expected < precision
 
 
 # Smoothed 0-1 Loss
 @pytest.mark.parametrize(
     "t_test, expected",
-    [(t, 0) if t > 1 else
-     (t, 1) if t < -1 else
-     (t, (1 / 4) * t ** 3 - (3 / 4) * t + 0.5)
+    [((t, 0), 0) if t > 1 else
+     ((t, 0), 1) if t < -1 else
+     ((t, 0), (1 / 4) * t ** 3 - (3 / 4) * t + 0.5)
      for t in rango_test]
 )
 def test_smooth01(t_test, expected):
-    assert fc.smooth01(t_test) - expected < precision
+    assert fc.smooth01(*t_test) - expected < precision
 
 
 @pytest.mark.parametrize(
     "t_test, expected",
-    [(t, 0) if t > 1 else
-     (t, 0) if t < -1 else
-     (t, (3 / 4) * (t ** 2 - 1))
+    [((t, 1), 0) if t > 1 else
+     ((t, 1), 0) if t < -1 else
+     ((t, 1), (3 / 4) * (t ** 2 - 1))
      for t in rango_test]
 )
 def test_derivative_smooth01(t_test, expected):
-    assert fc.derivative_smooth01(t_test) - expected < precision
+    assert fc.smooth01(*t_test) - expected < precision
+
+
+@pytest.mark.parametrize(
+    "t_test, expected",
+    [((t, 2), (3 / 2) * t) if -1 < t < 1 else
+     ((t, 2), 0.)
+     for t in rango_test]
+)
+def test_derivative2_smooth01(t_test, expected):
+    assert fc.smooth01(*t_test) - expected < precision
+
+
+@pytest.mark.parametrize(
+    "t_test, expected",
+    [((t, 3), 3 / 2) if -1 < t < 1 else
+     ((t, 3), 0.)
+     for t in rango_test]
+)
+def test_derivative3_smooth01(t_test, expected):
+    assert fc.smooth01(*t_test) - expected < precision
+
+
+@pytest.mark.parametrize(
+    "t_test, expected",
+    [((t, n), 0.)
+     for n in range(4, 10)
+     for t in rango_test]
+)
+def test_derivative_n_smooth01(t_test, expected):
+    assert fc.smooth01(*t_test) - expected < precision
 
 # Spline
